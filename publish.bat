2c477666772bacc521dd12a4d86ec06b424b7776
baott <baott@tinhvan.com>
2025-11-30 22:05:56 +0700

    update

--- publish.bat
+++ publish.bat
@@ -1,4 +1,7 @@
 @echo off
 dotnet pack -c Release
-dotnet nuget push bin/Release/Test.Package.1.0.6.nupkg --api-key %GITHUB_TOKEN% --source github
+
+REM Push any Test.Package.*.nupkg in Release
+dotnet nuget push "bin\Release\Test.Package.*.nupkg" --api-key %GITHUB_TOKEN% --source github
+
 echo Build and push completed successfully.
